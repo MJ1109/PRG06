c3c26fe22092b139ee6ab3db0f4d35200d2a2454
MJ1109 <manuja115@gmail.com>
2021-12-17 15:45:16 +0100

    -

--- routes/carsRoutes.js
+++ routes/carsRoutes.js
@@ -5,6 +5,14 @@ let routes = function() {
     //maak router aan
     let carsRouter = express.Router();
 
+    carsRouter.use((req, res, next) => {
+        if (req.headers.accept == "application/json"){
+            next()
+        } else {
+            res.status(400).send()
+        }
+    })
+
     carsRouter.route('/cars')
 
         .post(function (req, res){
@@ -99,71 +107,182 @@ let routes = function() {
             }).orFail()
         })
 
-        .put (function(req, res){
-            console.log("De put start hier");
+        // .put (function(req, res){
+        //     console.log("De put start hier");
 
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            res.header("Accept", "application/json, application/x-www-form-urlencoded");
+        //     res.header("Access-Control-Allow-Origin", "*");
+        //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //     res.header("Accept", "application/json, application/x-www-form-urlencoded");
 
-            if (req.body.title =="" || req.body.class =="" || req.body.year =="" || req.body.fuel =="" || req.body.power==""){
-                console.log("A field is empty!");
-                res.status(422).end();          
-            } 
-            else 
-            {
-                Car.findByIdAndUpdate(req.params.carId, req.body , function(err, car){
-                    if (err) {
-                        res.status(400).send(err);
-                    } 
-                    else
-                    {
-                        car.save(function(err){
-                            if (err) { 
-                                res.status(400).send(err) 
-                            }
-                            else 
-                            { 
-                                res.status(200).json(req.body)
-                            }
-                        });
+        //     if (req.body.title =="" || req.body.class =="" || req.body.year =="" || req.body.fuel =="" || req.body.power==""){
+        //         console.log("A field is empty!");
+        //         res.status(422).end();          
+        //     } 
+        //     else 
+        //     {
+        //         Car.findByIdAndUpdate(req.params.carId, req.body , function(err, car){
+
+        //             if (err) {
+        //                 res.status(400).send(err);
+        //             } 
+        //             else
+        //             {
+        //                 car.save(function(err){
+        //                     if (err) { 
+        //                         res.status(400).send(err) 
+        //                     }
+        //                     else 
+        //                     { 
+        //                         res.status(200).json(req.body)
+        //                         console.log(car);
+        //                     }
+        //                 });
+        //             }
+        //         }).orFail();
+        //     }
+        // })
+        //  .put (async function(req, res){
+        //     console.log("De put start hier");
+
+        //     currentCar = await Car.findById(req.params.carId)
+        //     res.header("Access-Control-Allow-Origin", "*");
+        //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //     res.header("Accept", "application/json, application/x-www-form-urlencoded");
+
+        //     if (currentCar == null){
+        //         console.log('car is null')
+        //         res.status(404).json({message: "car not found"})
+        //     }
+        //     console.log(currentCar)
+
+        //     if (req.body.title != null){
+        //         console.log(req.body.title);
+        //         currentCar.title = req.body.title
+        //     }
+        //     if (req.body.class != null){
+        //         currentCar.class = req.body.class
+        //     }
+        //     if (req.body.year != null){
+        //         currentCar.year = req.body.year
+        //     }
+        //     if (req.body.fuel != null){
+        //         currentCar.fuel = req.body.fuel
+        //     }
+        //     if (req.body.power != null){
+        //         currentCar.power = req.body.power
+        //     }
+
+        //     try {
+        //         const updatedCar = await currentCar.save()
+        //         console.log(updatedCar);
+        //         res.status(200).json(req.body.toJSON)
+        //     } catch (err) {
+        //         res.status(400).json({ message: err.message })
+        //     }
+        // })
+
+    //    ' // .delete (function(req, res){
+
+        //     console.log("Nu bezig met delete");
+
+        //    //show what it needs to delete
+        //     console.log(`DELETE on api/cars/${req.params.carId}`);
+
+        //     res.header("Access-Control-Allow-Origin", "*");
+        //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //     res.header("Accept", "application/json, application/x-www-form-urlencoded");
+
+        //     Car.findOneAndDelete({_id : req.params.carId}, function (err, car) {
+        //         if (err) {
+        //             res.status(400).send(err);
+        //         }
+        //         else
+        //         {
+        //             res.status(204).json(car);
+        //         }
+        //     }).orFail()
+        // })
+                // DELETE request
+                // .delete(function(req, res) {
+                //     console.log(`DELETE on api/cars/${req.params.carId}`);
+                //     console.log( req.params.carId);
+        
+                //     res.header("Access-Control-Allow-Origin", "*");
+                //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        
+                //     Car.findByIdAndDelete(req.params.carId, function (err, car) {
+                //         if (err) {
+                //             res.status(404).send(err);
+                //         }
+                //         else
+                //         {
+                //             res.status(204).json(car);
+                //         }
+                //     }).orFail()
+                // })
+
+                .put(getCar, async function(req, res){
+
+
+                    if (req.body.title != null){
+                         res.car.title = req.body.title
                     }
-                }).orFail();
-            }
-        })
+                    if (req.body.class != null){
+                        res.car.class = req.body.class
+                    }
+                    if (req.body.year != null){
+                        res.car.year = req.body.year
+                    }
+                    if (req.body.fuel != null){
+                        res.car.fuel = req.body.fuel
+                    }
+                    if (req.body.power != null){
+                        res.car.power = req.body.power
+                    }
+        
+                    try {
+                        const updatedCar = await currentCar.save()
+                        res.status(200).json(updatedCar)
+                    } catch (err) {
+                        res.status(400).json({ message: err.message })
+                    }
+                })
 
-        .delete (function(req, res){
+                .delete(getCar, async function(req, res){
+                    try {
+                        await res.car.remove()
+                        res.status(204).json({ message: 'car successfully deleted' })
+                    } catch (err) {
+                        res.send(500).json({ message: err.message})
+                    }
+                }) 
+                
+                .options(function(req, res){
 
-            console.log("Nu bezig met delete");
-
-           //show what it needs to delete
-            console.log(`DELETE on api/cars/${req.params.carId}`);
-
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            res.header("Accept", "application/json, application/x-www-form-urlencoded");
-
-            Car.findOneAndDelete({_id : req.params.carId}, function (err, car) {
-                if (err) {
-                    res.status(400).send(err);
+                    console.log("Options tes op detailresource");
+        
+                    res.header("Accept", "application/json", "application/x-www-form-urlencoded");
+                    res.header("Acces-Control_Allow-Origin", "*");
+                    res.header("Acces-Control_Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
+                    res.header("Allow", "GET,OPTIONS,PUT,DELETE");
+                    res.header("Access-Control-Allow-Methods", "GET,PUT,DELETE,OPTIONS").send();
+                });
+                
+                async function getCar (req, res, next){
+                    let car
+                    try {
+                            car = await Car.findById(req.params.id)
+                            if(car == null){
+                                return res.status(404).json({
+                                    message: "Car not found"
+                                })
+                            }        
+                    } catch (err) {
+                        return res.status(500).json({ message: err.message})
+                    }
+                    res.car = car
+                    next()
                 }
-                else
-                {
-                    res.status(204).json(car);
-                }
-            }).orFail()
-        })
-
-        .options(function(req, res){
-
-            console.log("Options tes op detailresource");
-
-            res.header("Accept", "application/json", "application/x-www-form-urlencoded");
-            res.header("Acces-Control_Allow-Origin", "*");
-            res.header("Acces-Control_Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
-            res.header("Allow", "GET,OPTIONS,PUT,DELETE");
-            res.header("Access-Control-Allow-Methods", "GET,PUT,DELETE,OPTIONS").send();
-        });
 
     return carsRouter;   
 }
